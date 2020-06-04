45638bd88b9db6ab087f1b192272f4902d92a864
Agamendon <48686843+Agamendon@users.noreply.github.com>
2020-06-04 17:08:10 +0300

    04.06.2020 17.08

--- Server.py
+++ Server.py
@@ -2,4 +2,19 @@ import socketserver
 import threading
 
 
-class ThreadedTCPServer:
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        data = self.request.recv(1024)
+
+
+if __name__ == '__main__':
+    ip = 'localhost'
+    port = 27036
+    with ThreadedTCPServer((ip, port), ThreadedTCPRequestHandler) as server:
+        server_thread = threading.Thread(target=server.serve_forever())
+        server_thread.daemon = True
+        server_thread.start()
